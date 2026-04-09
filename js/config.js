d6518ba61688017d03944ae00812c771ede910e5
daytana1013-hash <daytana1013@gmail.com>
2026-04-09 22:56:12 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -7,5 +7,5 @@ const CONFIG = {
   jsonPath: 'data/db.json',
   notionApiKey: '',
   notionDbId: '33d4299f7b1d802d9ac7c3067be965a2',
-  gasProxyUrl: 'gasProxyUrl: 'https://script.google.com/macros/s/https://script.google.com/macros/s/AKfycbwhiYYdzJesAEIPyVNB7Ym4lfooBHul66u0__Sv3CAeJUGc1nrMQbUfbzNv_Q-JJTC7/exec/exec',',
+  gasProxyUrl: 'https://script.google.com/macros/s/https://script.google.com/macros/s/AKfycbwhiYYdzJesAEIPyVNB7Ym4lfooBHul66u0__Sv3CAeJUGc1nrMQbUfbzNv_Q-JJTC7/exec/exec',',
 };
